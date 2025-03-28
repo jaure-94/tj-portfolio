25cc8e213fb8658263ea0fcd8c34a74ac3891d0d
Tawanda Jaure <tawandajaujau@gmail.com>
2025-03-28 10:10:09 +0200

    edit contact for animations

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,6 +3,7 @@
 import { useForm } from 'react-hook-form'
 import { DevTool } from '@hookform/devtools'
 import { useEffect, useState } from 'react'
+import Reveal from '@/animations/Reveal'
 
 interface FormValues {
   username: string
@@ -37,23 +38,24 @@ const ContactForm = () => {
         onSubmit={handleSubmit(onSubmit)}
         noValidate
         >
+        <Reveal>
         <div className='flex flex-col gap-5 md:flex-row md:justify-between'>
           <div className='flex flex-col w-full gap-2'>
-            <label htmlFor="username">Username</label>
-            <input
-              placeholder='John Doe'
-              className='form-input'
-              type="text"
-              id="username"
-              {...register('username', {
-                required: "*Username is required"
-              })}
-            />
+              <label htmlFor="username">Username</label>
+              <input
+                placeholder='John Doe'
+                className='form-input'
+                type="text"
+                id="username"
+                {...register('username', {
+                  required: "*Username is required"
+                })}
+              />
             <p className='text-red-500 text-sm'>{errors.username?.message}</p>
           </div>
-          
+        
           <div className='flex flex-col w-full gap-2'>
-            <label htmlFor="email">Email</label>
+              <label htmlFor="email">Email</label>
             <input
               placeholder='john@example.com'
               className='form-input'
@@ -70,28 +72,33 @@ const ContactForm = () => {
             <p className='text-red-500 text-sm'>{errors.email?.message}</p>
           </div>
         </div>
+        </Reveal>
         
-        <div className='flex flex-col gap-2'>
-          <label htmlFor="mutupo">Message</label>
-          <input
-            placeholder='Type your message here...'
-            className='textarea'
-            type="text"
-            id="message"
-            {...register('message', {
-              required: "*Message is required"
-            })}
-          />
-          <p className='text-red-500 text-sm'>{errors.message?.message}</p>
-        </div>
+        <Reveal>
+          <div className='flex flex-col gap-2'>
+            <label htmlFor="mutupo">Message</label>
+            <input
+              placeholder='Type your message here...'
+              className='textarea'
+              type="text"
+              id="message"
+              {...register('message', {
+                required: "*Message is required"
+              })}
+            />
+            <p className='text-red-500 text-sm'>{errors.message?.message}</p>
+          </div>
+        </Reveal>
 
-        <button
-          className='px-4 py-3 w-64 rounded-md text-white bg-stone-900 hover:bg-stone-700 transition-all duration-200'
-          type="submit"
-          disabled={ isSubmitting }
-          >
-          {isSubmitting ? "Submitting..." : "Submit"}
-        </button>
+        <Reveal>
+          <button
+            className='px-4 py-3 w-64 rounded-md text-white bg-stone-900 hover:bg-stone-700 transition-all duration-200'
+            type="submit"
+            disabled={ isSubmitting }
+            >
+            {isSubmitting ? "Submitting..." : "Submit"}
+          </button>
+        </Reveal>
 
         {successMessage && (<div className='px-14 py-8 rounded-md bg-green-300'>
           Message successfully sent! We&apos;ll reach out soon...
